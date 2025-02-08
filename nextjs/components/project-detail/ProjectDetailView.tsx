19dcf6561bf4cf210344868d3c06576f0747b4d3
Wesman687 <wesman687@gmail.com>
2025-02-08 04:31:10 -0500

    one more

--- nextjs/components/project-detail/ProjectDetailView.tsx
+++ nextjs/components/project-detail/ProjectDetailView.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import ProjectDetailStepper from './ProjectDetailStepper';
 import ProjectDetailBody from './ProjectDetailBody';
 import ProjectDetailHeader from './ProjectDetailHeader';
-import { Project } from '@/server/db/schema';
 
 // interface ProjectPageProps {
 //   project: Project
