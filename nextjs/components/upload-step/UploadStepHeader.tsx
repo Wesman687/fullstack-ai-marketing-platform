63d184b67659125d25998b3938a5ba5d81f0f12f
Wesman687 <wesman687@gmail.com>
2025-02-08 21:44:39 -0500

    pushing to reload production environment

--- nextjs/components/upload-step/UploadStepHeader.tsx
+++ nextjs/components/upload-step/UploadStepHeader.tsx
@@ -6,6 +6,7 @@ function UploadStepHeader() {
         <h2 className='text-xl md:text-2xl lg:text-2xl font-bold mb-8'>Step 1: Upload Media</h2>
         <div className='p-10 border-2 border-dashed border-main bg-white rounded-3xl text-center cursor-pointer mb-10'>
 
+
         </div>
     </div>
   )
