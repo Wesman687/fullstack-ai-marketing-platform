c932084f039b11e54d765879c7d80afabe695682
Wesman687 <wesman687@gmail.com>
2025-02-21 14:17:17 -0500

    forcing edeploy

--- nextjs/lib/config.ts
+++ nextjs/lib/config.ts
@@ -10,3 +10,4 @@ export const DGO_DATABASE = process.env.DGO_DATABASE;
 export const DGO_PORT = process.env.DGO_PORT;
 export const DGO_SSL = process.env.DATABASE_SSL;
 export const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET
+
